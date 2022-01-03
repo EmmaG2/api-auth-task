4ec855c57a9a07eb89a7df7e13f857c76fb63874
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-01-02 23:06:26 -0600

    CORS implemented

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 // crear el server
-
 const app = express();
 const port = 3000;
+
+// CORS
+app.use( cors() );
 
 // Rutas
 app.use( '/api/auth', require( './routes/auth.js' ) );
